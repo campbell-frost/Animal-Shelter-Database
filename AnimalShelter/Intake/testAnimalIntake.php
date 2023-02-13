a76e41ae555163b6887d8a87a0fa3b62abbf096d
Ronisha21 <68798465+Ronisha21@users.noreply.github.com>
2023-02-13 00:57:27 -0500

    Update and rename animalIntake.html to testAnimalIntake.php

---
+++ AnimalShelter/Intake/testAnimalIntake.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Insert form value into database</title>
+</head>
+<body>
+    <form action="testAnimalIntakeInsert.php" method="post">
+        <label>Animal Type:</label><input type="text" name="type"><br>
+        <label>Animal Name:</label><input type="text" name="name"><br>
+        <label>Sex:</label><input type="text" name="sex"><br>
+        <label>Breed:</label><input type="text" name="breed"><br>
+        <label>Color:</label><input type="text" name="color"><br>
+        <label>Weight:</label><input type="text" name="weight"><br>
+        <label>Altered:</label><input type="text" name="altered"><br>
+        <label>Microchip:</label><input type="text" name="microchip"><br>
+        <label>Address:</label><input type="text" name="address"><br> 
+        <label>City:</label><input type="text" name="city"><br>
+        <label>State:</label><input type="text" name="state"><br>
+        <label>Rabies Vaccination:</label><input type="text" name="rabiesVacc"><br>
+        <label>Year:</label><input type="text" name="rabiesYear"><br>
+        <label>Distemper Vaccination:</label><input type="text" name="distemperVacc"><br>
+        <label>Year:</label><input type="text" name="distemperYear"><br>
+        <button type="submit" name="submit">Submit</button>
+    </form>
+</body>
+</html>
+
+
