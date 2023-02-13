85150a80f5a954138c93c934cf6d0c4d819bf227
Ronisha21 <68798465+Ronisha21@users.noreply.github.com>
2023-02-13 10:57:05 -0500

    Update testAnimalIntakeInsert.php

--- AnimalShelter/Intake/testAnimalIntakeInsert.php
+++ AnimalShelter/Intake/testAnimalIntakeInsert.php
@@ -9,29 +9,39 @@ $conn = mysqli_connect($server , $username , $password , $dbname);
 // Condition when the user clicks submit on the form
 if(isset($_POST['submit']))
 {
-    if(!empty($_POST['name']) && !empty($_POST['breed']) && !empty($_POST['color']) && !empty($_POST['address']))
+    if(!empty($_POST['name']) && !empty($_POST['breed']) && !empty($_POST['color']) && !empty($_POST['type']))
     {
         // Values entered in the form by the user
+        $date = $_POST['date'] ;
         $type = $_POST['type'] ;
         $name = $_POST['name'] ;
+        $dateOfBirth = $_POST['dateOfBirth'] ;
         $sex = $_POST['sex'] ;
         $breed = $_POST['breed'] ;
         $color= $_POST['color'] ;
         $weight= $_POST['weight'] ;
         $altered= $_POST['altered'] ;
         $microchip= $_POST['microchip'] ;
+        $broughtIn = $_POST['broughtIn'] ;
+        $location = $_POST['location'];
+        $owner = $_POST['owner'] ;
+        $phone = $_POST['phone'] ;
         $address = $_POST['address'] ;
         $city = $_POST['city'] ;
         $state = $_POST['state'] ;
+        $zipcode = $_POST['zipcode'] ;
         $rabiesVacc = $_POST['rabiesVacc'] ;
         $rabiesYear = $_POST['rabiesYear'] ;
         $distemperVacc = $_POST['distemperVacc'] ;
         $distemperYear = $_POST['distemperYear'] ;
+        $spayedNeutered = $_POST['spayedNeutered'] ;
+        $tagNumber = $_POST['tagNumber'] ;
+        $clinic = $_POST['clinic'] ;
 
         // Form a query passing in the values from the user to the form attributes
-        $query = "insert into animalintake(type, name, sex, breed, color, weight, altered, microchip, address, city, state, rabiesVacc, rabiesYear, 
-        distemperVacc, distemperYear) values('$type', '$name', '$sex', '$breed', '$color', '$weight', '$altered', '$microchip', '$address', '$city', 
-        '$state', '$rabiesVacc', '$rabiesYear', '$distemperVacc', '$distemperYear')";
+        $query = "insert into animalintake(date, type, name, dateOfBirth, sex, breed, color, weight, altered, microchip, broughtIn, location, owner, phone, address, city, state, zipcode, rabiesVacc, rabiesYear, 
+        distemperVacc, distemperYear, spayedNeutered, tagNumber, clinic) values('$date', '$type', '$name', '$dateOfBirth', '$sex', '$breed', '$color', '$weight', '$altered', '$microchip', '$broughtIn', '$location', '$owner', '$phone', '$address', '$city', 
+        '$state', '$zipcode', '$rabiesVacc', '$rabiesYear', '$distemperVacc', '$distemperYear', '$spayedNeutered', '$tagNumber', '$clinic')";
 
         // Incase query will not run it will throw an error
         $run = mysqli_query($conn, $query) or die(mysqli_error());
