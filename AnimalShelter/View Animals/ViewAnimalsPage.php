e3f275bcb7656f534930e1e370cba7a049cb2c50
JustTyler <tylerard9@gmail.com>
2023-02-14 21:54:24 -0500

    Update ViewAnimalsPage.php

--- AnimalShelter/View Animals/ViewAnimalsPage.php
+++ AnimalShelter/View Animals/ViewAnimalsPage.php
@@ -1,5 +1,35 @@
 <?php
 include("viewAnimals.php");
+
+//if(!empty($_GET['name']) && !empty($_GET['breed']) && !empty($_GET['color']) && !empty($_GET['type']))
+  //  {
+        // Values retrieved from the 'animalintake' table
+        $date = $_GET['date'] ;
+        $type = $_GET['type'] ;
+        $name = $_GET['name'] ;
+        $dateOfBirth = $_GET['dateOfBirth'] ;
+        $sex = $_GET['sex'] ;
+        $breed = $_GET['breed'] ;
+        $color= $_GET['color'] ;
+        $weight= $_GET['weight'] ;
+        $altered= $_GET['altered'] ;
+        $microchip= $_GET['microchip'] ;
+        $broughtIn = $_GET['broughtIn'] ;
+        $location = $_GET['location'];
+        $owner = $_GET['owner'] ;
+        $phone = $_GET['phone'] ;
+        $address = $_GET['address'] ;
+        $city = $_GET['city'] ;
+        $state = $_GET['state'] ;
+        $zipcode = $_GET['zipcode'] ;
+        $rabiesVacc = $_GET['rabiesVacc'] ;
+        $rabiesYear = $_GET['rabiesYear'] ;
+        $distemperVacc = $_GET['distemperVacc'] ;
+        $distemperYear = $_GET['distemperYear'] ;
+        $spayedNeutered = $_GET['spayedNeutered'] ;
+        $tagNumber = $_GET['tagNumber'] ;
+        $clinic = $_GET['clinic'] ;
+  //  }
 ?>
 
 <!DOCTYPE html>
@@ -28,7 +58,20 @@ include("viewAnimals.php");
             <!--Adds each characteristic as a header for each animal's attributes in the list-->
             <h4>&emsp;Name&emsp;&emsp;&emsp;&emsp;Image&emsp;&emsp;&emsp;&emsp;ID&emsp;&emsp;&emsp;&emsp;Cage&emsp;&emsp;&emsp;&emsp;Type&emsp;&emsp;&emsp;&emsp;Breed&emsp;&emsp;&emsp;&emsp;Sex&emsp;&emsp;&emsp;&emsp;Age&emsp;&emsp;&emsp;&emsp;Color&emsp;&emsp;&emsp;&emsp;Brought In</h4>
             <hr>
-         <!--Placeholder animal-->   <p>Mittens&emsp;&emsp;&emsp;&emsp;<img src = "grayCat.jpg">&emsp;&emsp;&emsp;&emsp;001&emsp;&emsp;&emsp;&emsp;13&emsp;&emsp;&emsp;&emsp;Cat&emsp;&emsp;&emsp;&emsp;Breed&emsp;&emsp;&emsp;&emsp;F&emsp;&emsp;&emsp;&emsp;1 yr 0 m&emsp;&emsp;&emsp;&emsp;Gray&emsp;&emsp;&emsp;&emsp;11/17/22</p>
+            <?php
+                $sql = "SELECT * FROM animals;" ;
+                $results = mysqli_query($conn, $sql) ;
+                $resultsCheck = mysqli_num_rows($results) ;
+
+                if($resultsCheck > 0)
+                {
+                    while($row = mysqli_fetch_assoc($results))
+                    {
+                        echo $row['date'] ;
+                    }
+                }
+            ?>
+         <!--Placeholder animal-->  <!-- <p>Mittens&emsp;&emsp;&emsp;&emsp;<img src = "grayCat.jpg">&emsp;&emsp;&emsp;&emsp;001&emsp;&emsp;&emsp;&emsp;13&emsp;&emsp;&emsp;&emsp;Cat&emsp;&emsp;&emsp;&emsp;Breed&emsp;&emsp;&emsp;&emsp;F&emsp;&emsp;&emsp;&emsp;1 yr 0 m&emsp;&emsp;&emsp;&emsp;Gray&emsp;&emsp;&emsp;&emsp;11/17/22</p> -->
         </div>
     </body>
 </html>
