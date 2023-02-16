9213c025dc3db27943752c04f48b3d69930092f4
JustTyler <tylerard9@gmail.com>
2023-02-16 12:18:59 -0500

    Update ViewAnimalsPage.php

--- AnimalShelter/View Animals/ViewAnimalsPage.php
+++ AnimalShelter/View Animals/ViewAnimalsPage.php
@@ -55,21 +55,54 @@ include("viewAnimals.php");
         <div class = "center">
             <h1>View Animals</h1>
             <h2>Results</h2>
-            <!--Adds each characteristic as a header for each animal's attributes in the list-->
-            <h4>&emsp;Name&emsp;&emsp;&emsp;&emsp;Image&emsp;&emsp;&emsp;&emsp;ID&emsp;&emsp;&emsp;&emsp;Cage&emsp;&emsp;&emsp;&emsp;Type&emsp;&emsp;&emsp;&emsp;Breed&emsp;&emsp;&emsp;&emsp;Sex&emsp;&emsp;&emsp;&emsp;Age&emsp;&emsp;&emsp;&emsp;Color&emsp;&emsp;&emsp;&emsp;Brought In</h4>
-            <hr>
+            <!--Adds each characteristic as a header for each animal's attributes in the list as a table-->
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Date</th>
+                    <th>Date Of Birth</th>
+                    <th>Sex</th>
+                    <th>Breed</th>
+                    <th>Color</th>
+                    <th>Weight</th>
+                    <th>Altered</th>
+                    <th>Microchip</th>
+                    <th>Brought In</th>
+                    <th>Location</th>
+                    <th>Owner</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>City</th>
+                    <th>State</th>
+                    <th>Zipcode</th>
+                    <th>Rabies Vacc</th>
+                    <th>Rabies Year</th>
+                    <th>Distemper Vacc</th>
+                    <th>Distemper Year</th>
+                    <th>Spayed/Neutered</th>
+                    <th>Tag Number</th>
+                    <th>Clinic</th>
+                </tr>
+            </table>
             <?php
                 $sql = "SELECT * FROM animals;" ;
-                $results = mysqli_query($conn, $sql) ;
-                $resultsCheck = mysqli_num_rows($results) ;
+                //$results = mysqli_query($conn, $sql) ;
+                $results = $conn->query($sql) ;
+                //$resultsCheck = mysqli_num_rows($results) ;
 
-                if($resultsCheck > 0)
+                if($results-> num_rows > 0)
                 {
-                    while($row = mysqli_fetch_assoc($results))
+                    while($row = $results-> fetch_assoc())
                     {
-                        echo $row['date'] ;
+                        echo "<tr><td>" . row["name"] . "</td><td>" . row["type"] . "</td><td>" . row["date"] . "</td><td>" . row["dateOfBirth"] ;//. "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>";
                     }
                 }
+                else
+                {
+                    echo "No results";
+                }
+                $conn->close();
             ?>
          <!--Placeholder animal-->  <!-- <p>Mittens&emsp;&emsp;&emsp;&emsp;<img src = "grayCat.jpg">&emsp;&emsp;&emsp;&emsp;001&emsp;&emsp;&emsp;&emsp;13&emsp;&emsp;&emsp;&emsp;Cat&emsp;&emsp;&emsp;&emsp;Breed&emsp;&emsp;&emsp;&emsp;F&emsp;&emsp;&emsp;&emsp;1 yr 0 m&emsp;&emsp;&emsp;&emsp;Gray&emsp;&emsp;&emsp;&emsp;11/17/22</p> -->
         </div>
