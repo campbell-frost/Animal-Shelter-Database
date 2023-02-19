bae8a1d17e947225b88a8159f538bd5536b6a39d
JustTyler <tylerard9@gmail.com>
2023-02-19 18:59:06 -0500

    Update ViewAnimalsPage.php

--- AnimalShelter/View Animals/ViewAnimalsPage.php
+++ AnimalShelter/View Animals/ViewAnimalsPage.php
@@ -1,10 +1,9 @@
 <?php
 include("viewAnimals.php");
 
-//if(!empty($_GET['name']) && !empty($_GET['breed']) && !empty($_GET['color']) && !empty($_GET['type']))
-  //  {
+if(!empty($_GET['name']) && !empty($_GET['breed']) && !empty($_GET['color']) && !empty($_GET['type']) && !empty($_GET['date']) && !empty($_GET['dateOfBirth']) && !empty($_GET['sex']) && !empty($_GET['color']) && !empty($_GET['weight']) && !empty($_GET['altered']) && !empty($_GET['microchip']) && !empty($_GET['broughtIn']) && !empty($_GET['location']) && !empty($_GET['owner']) && !empty($_GET['phone']) && !empty($_GET['adress']) && !empty($_GET['city']) && !empty($_GET['state']) && !empty($_GET['zipcode']) && !empty($_GET['rabiesVacc']) && !empty($_GET['rabiesYear']) && !empty($_GET['distemperVacc']) && !empty($_GET['distemperYear']) && !empty($_GET['spayedNeutered']) && !empty($_GET['tagNumber']) && !empty($_GET['clinic']))
+    {
         // Values retrieved from the 'animalintake' table
-        $date = $_GET['date'] ;
         $type = $_GET['type'] ;
         $name = $_GET['name'] ;
         $dateOfBirth = $_GET['dateOfBirth'] ;
@@ -12,24 +11,11 @@ include("viewAnimals.php");
         $breed = $_GET['breed'] ;
         $color= $_GET['color'] ;
         $weight= $_GET['weight'] ;
-        $altered= $_GET['altered'] ;
-        $microchip= $_GET['microchip'] ;
-        $broughtIn = $_GET['broughtIn'] ;
-        $location = $_GET['location'];
-        $owner = $_GET['owner'] ;
-        $phone = $_GET['phone'] ;
-        $address = $_GET['address'] ;
-        $city = $_GET['city'] ;
-        $state = $_GET['state'] ;
-        $zipcode = $_GET['zipcode'] ;
         $rabiesVacc = $_GET['rabiesVacc'] ;
         $rabiesYear = $_GET['rabiesYear'] ;
-        $distemperVacc = $_GET['distemperVacc'] ;
-        $distemperYear = $_GET['distemperYear'] ;
         $spayedNeutered = $_GET['spayedNeutered'] ;
         $tagNumber = $_GET['tagNumber'] ;
-        $clinic = $_GET['clinic'] ;
-  //  }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -60,29 +46,15 @@ include("viewAnimals.php");
                 <tr>
                     <th>Name</th>
                     <th>Type</th>
-                    <th>Date</th>
                     <th>Date Of Birth</th>
                     <th>Sex</th>
                     <th>Breed</th>
                     <th>Color</th>
                     <th>Weight</th>
-                    <th>Altered</th>
-                    <th>Microchip</th>
-                    <th>Brought In</th>
-                    <th>Location</th>
-                    <th>Owner</th>
-                    <th>Phone</th>
-                    <th>Address</th>
-                    <th>City</th>
-                    <th>State</th>
-                    <th>Zipcode</th>
                     <th>Rabies Vacc</th>
                     <th>Rabies Year</th>
-                    <th>Distemper Vacc</th>
-                    <th>Distemper Year</th>
                     <th>Spayed/Neutered</th>
                     <th>Tag Number</th>
-                    <th>Clinic</th>
                 </tr>
             </table>
             <?php
@@ -91,11 +63,13 @@ include("viewAnimals.php");
                 $results = $conn->query($sql) ;
                 //$resultsCheck = mysqli_num_rows($results) ;
 
+                // Checks that there is still results to add to the row
                 if($results-> num_rows > 0)
                 {
+                // Shows the parameters of animals from the database
                     while($row = $results-> fetch_assoc())
                     {
-                        echo "<tr><td>" . row["name"] . "</td><td>" . row["type"] . "</td><td>" . row["date"] . "</td><td>" . row["dateOfBirth"] ;//. "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>""<tr><td>" . row["name"] . "</td><td>";
+                        echo "<tr><td>" . $row["name"] . "</td><td>" . $row["type"] . "</td><td>" . $row["dateOfBirth"] . "</td><td>" . $row["sex"] . "</td><td>" . $row["breed"] . "</td><td>" . $row["color"] . "</td><td>" . $row["weight"] . "</td><td>" . $row["rabiesVacc"] . "</td><td>" . "</td><td>" . $row["rabiesYear"] . "</td><td>" . "</td><td>" . $row["spayedNeutered"] . "</td><td>" . "</td><td>" . $row["tagNumber"] . "</td><td>";
                     }
                 }
                 else
