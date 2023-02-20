6b2e649d886f56b4be4fdd0f2838d08e66d9c02b
JustTyler <tylerard9@gmail.com>
2023-02-19 20:02:19 -0500

    Update ViewAnimalsPage.php

--- AnimalShelter/View Animals/ViewAnimalsPage.php
+++ AnimalShelter/View Animals/ViewAnimalsPage.php
@@ -1,7 +1,7 @@
 <?php
 include("viewAnimals.php");
 
-if(!empty($_GET['name']) && !empty($_GET['breed']) && !empty($_GET['color']) && !empty($_GET['type']) && !empty($_GET['date']) && !empty($_GET['dateOfBirth']) && !empty($_GET['sex']) && !empty($_GET['color']) && !empty($_GET['weight']) && !empty($_GET['altered']) && !empty($_GET['microchip']) && !empty($_GET['broughtIn']) && !empty($_GET['location']) && !empty($_GET['owner']) && !empty($_GET['phone']) && !empty($_GET['adress']) && !empty($_GET['city']) && !empty($_GET['state']) && !empty($_GET['zipcode']) && !empty($_GET['rabiesVacc']) && !empty($_GET['rabiesYear']) && !empty($_GET['distemperVacc']) && !empty($_GET['distemperYear']) && !empty($_GET['spayedNeutered']) && !empty($_GET['tagNumber']) && !empty($_GET['clinic']))
+if(!empty($_GET['name']) && !empty($_GET['breed']) && !empty($_GET['color']) && !empty($_GET['type']) && !empty($_GET['dateOfBirth']) && !empty($_GET['sex']) && !empty($_GET['weight']) && !empty($_GET['rabiesVacc']) && !empty($_GET['rabiesYear']) && !empty($_GET['spayedNeutered']) && !empty($_GET['tagNumber']))
     {
         // Values retrieved from the 'animalintake' table
         $type = $_GET['type'] ;
@@ -30,17 +30,38 @@ if(!empty($_GET['name']) && !empty($_GET['breed']) && !empty($_GET['color']) && 
         <!--Gives the file the name View Animals-->
         <title>View Animals</title>
 
-        <link rel="stylesheet" type="text/css" href="styles.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     
     <body>
+        
         <div id="search-bar">
             <input type="text" placeholder="Search...">
           </div>
           <!--Displays "View Animals" in the top-center of the page and Results just below it-->
-        <div class = "center">
+       <!-- <div class = "center"> -->
+     <!--  <div id="banner"> -->
+    <!-- <div id="title">View Animal</div>
+    <div id="search-bar">
+      <input type="text" placeholder="Search..."></div>
+      
+    </div> 
+    <nav>
+      <ul>
+        <li><a href="animal-intake.php">Animal Intake</a></li>
+        <li><a href="view-animals.php">View Animals</a></li>
+        <li><a href="reclaim-animals.php">Reclaim Animals</a></li> 
+        <li><a href="reports.php">Reports</a></li> 
+        <li><a href="incident-reports.php">File Incident Report</a></li> 
+      <li><a href="dispositions.php">Dispositions</a></li> 
+      <li><a href="fees-reciept.php">Fees Receipt</a></li> 
+      
+    </ul>
+  </nav> -->
+    </div>
             <h1>View Animals</h1>
             <h2>Results</h2>
+    <div id="body">
             <!--Adds each characteristic as a header for each animal's attributes in the list as a table-->
             <table>
                 <tr>
@@ -79,6 +100,7 @@ if(!empty($_GET['name']) && !empty($_GET['breed']) && !empty($_GET['color']) && 
                 $conn->close();
             ?>
          <!--Placeholder animal-->  <!-- <p>Mittens&emsp;&emsp;&emsp;&emsp;<img src = "grayCat.jpg">&emsp;&emsp;&emsp;&emsp;001&emsp;&emsp;&emsp;&emsp;13&emsp;&emsp;&emsp;&emsp;Cat&emsp;&emsp;&emsp;&emsp;Breed&emsp;&emsp;&emsp;&emsp;F&emsp;&emsp;&emsp;&emsp;1 yr 0 m&emsp;&emsp;&emsp;&emsp;Gray&emsp;&emsp;&emsp;&emsp;11/17/22</p> -->
+        <!-- </div> -->
         </div>
     </body>
 </html>
