dd660ef3d911cfdcedbc7ac65e34eb19d8656b2d
Ronisha21 <68798465+Ronisha21@users.noreply.github.com>
2023-02-27 19:38:56 -0500

    Update testAnimalIntake.php

--- AnimalShelter/Intake/testAnimalIntake.php
+++ AnimalShelter/Intake/testAnimalIntake.php
@@ -5,7 +5,10 @@
 </head>
 <body>
     <form action="testAnimalIntakeInsert.php" method="post">
-        <label>Today's Date:</label><input type="date" name="date"><br>
+        <!-- Date field for current date entered by selecting a date in a calender -->
+        <label>Date:</label><input type="date" name="date"><br>
+        
+        <!-- Dropdown selection for selecting animal type -->
         <label>Animal Type:</label>
         <select name="type">
             <option value=""></option>
@@ -13,23 +16,39 @@
             <option value="Cat">Cat</option>
             <option value="Livestock">Livestock</option>
         </select><br>
+        
+        <!-- Animal name field entered in a textbox -->
         <label>Animal Name:</label><input type="text" name="name"><br>
+        
+        <!-- Date of birth field entered by selecting a date in a calender -->
         <label>Date of Birth:</label><input type="date" name="dateOfBirth"><br>
+        
+        <!-- Dropdown selection for selecting animal sex -->
         <label>Sex:</label>
         <select name="sex">
             <option value=""></option>
             <option value="M">M</option>
             <option value="F">F</option>
         </select><br>
+        
+        <!-- Breed of animal field entered in a textbox -->
         <label>Breed:</label><input type="text" name="breed"><br>
+        
+        <!-- Color of animal field entered in a textbox -->
         <label>Color:</label><input type="text" name="color"><br>
+        
+        <!-- Weight of animal field entered in a textbox -->
         <label>Weight:</label><input type="text" name="weight"><br>
+        
+        <!-- Dropdown selection for selecting whether animal has been altered -->
         <label>Altered:</label>
         <select name="altered">
             <option value=""></option>
             <option value="Y">Y</option>
             <option value="N">N</option>
         </select><br>
+        
+        <!-- Dropdown selection for selecting whether animal has a microchip -->
         <label>Microchip:</label>
         <select name="microchip">
             <option value=""></option>
@@ -39,6 +58,7 @@
         <label>Brought in by ACO (badge number) or Citizen:</label><input type="text" name="broughtIn"><br>
         <label>Location:</label><input type="text" name="location">
         <p>(Shelter Cage number or Veterinarian number if animal is housed off-site)</p><br>
+        <!-- Dropdown selection for selecting whether animal has had rabies vaccination -->
         <label>Rabies Vaccination:</label>
         <select name="rabiesVacc">
             <option value=""></option>
@@ -46,6 +66,7 @@
             <option value="N">N</option>
         </select><br>
         <label>Year:</label><input type="text" name="rabiesYear"><br>
+        <!-- Dropdown selection for selecting whether animal has had distemper vaccination -->
         <label>Distemper Vaccination:</label>
         <select name="distemperVacc">
             <option value=""></option>
@@ -53,6 +74,7 @@
             <option value="N">N</option>
         </select><br>
         <label>Year:</label><input type="text" name="distemperYear"><br>
+        <!-- Dropdown selection for selecting whether animal has been spayed or neutered -->
         <label>Spayed/Neutered:</label><input type="text" name="spayedNeutered"><br>
         <select name="spayedNeutered">
             <option value=""></option>
