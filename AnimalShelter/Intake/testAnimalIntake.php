2ac4bfff79df667786aa9c8c0a3944049b6a3337
Ronisha21 <68798465+Ronisha21@users.noreply.github.com>
2023-02-27 19:45:12 -0500

    Update testAnimalIntake.php

--- AnimalShelter/Intake/testAnimalIntake.php
+++ AnimalShelter/Intake/testAnimalIntake.php
@@ -55,9 +55,16 @@
             <option value="Y">Y</option>
             <option value="N">N</option>
         </select><br>
+     
+         <!-- Field indicating whether animal has been brought in by an animal control
+        officer or citizen which is entered in a textbox -->
         <label>Brought in by ACO (badge number) or Citizen:</label><input type="text" name="broughtIn"><br>
+       
+        <!-- Location of animal field entered in a textbox which the text below indicates 
+        the desired input if animal is housed at the animal shelter or off-site -->
         <label>Location:</label><input type="text" name="location">
         <p>(Shelter Cage number or Veterinarian number if animal is housed off-site)</p><br>
+        
         <!-- Dropdown selection for selecting whether animal has had rabies vaccination -->
         <label>Rabies Vaccination:</label>
         <select name="rabiesVacc">
@@ -65,7 +72,10 @@
             <option value="Y">Y</option>
             <option value="N">N</option>
         </select><br>
+        
+        <!-- Year of rabies vaccination entered in a textbox -->
         <label>Year:</label><input type="text" name="rabiesYear"><br>
+        
         <!-- Dropdown selection for selecting whether animal has had distemper vaccination -->
         <label>Distemper Vaccination:</label>
         <select name="distemperVacc">
@@ -73,6 +83,8 @@
             <option value="Y">Y</option>
             <option value="N">N</option>
         </select><br>
+        
+        <!-- Year of distemper vaccination entered in a textbox -->
         <label>Year:</label><input type="text" name="distemperYear"><br>
         <!-- Dropdown selection for selecting whether animal has been spayed or neutered -->
         <label>Spayed/Neutered:</label><input type="text" name="spayedNeutered"><br>
@@ -81,15 +93,35 @@
             <option value="Y">Y</option>
             <option value="N">N</option>
         </select><br>
+        
+        <!-- Tagnumber of animal field entered in a textbox -->
         <label>Tag Number:</label><input type="text" name="tagNumber"><br>
+       
+        <!-- Clinic of animal field entered in a textbox -->
         <label>Clinic:</label><input type="text" name="clinic"><br>
+        
+        <!-- Image of animal field uploaded from the local computer as a file -->
         <label>Image:</label><input type="file" name="file"><br>
+        
+        <!-- Name of owner of animal field entered in a textbox -->
         <label>Owner(if known):</label><input type="text" name="owner"><br>
+        
+        <!-- Phone number of animal owner field entered in a textbox -->
         <label>Phone Number:</label><input type="text" name="phone"><br> 
+        
+        <!-- Address of animal owner field entered in a textbox -->
         <label>Address:</label><input type="text" name="address"><br> 
+        
+        <!-- City of animal owner field entered in a textbox -->
         <label>City:</label><input type="text" name="city"><br>
+        
+        <!-- State of animal owner field entered in a textbox -->
         <label>State:</label><input type="text" name="state"><br>
+        
+        <!-- Zipcode of animal owner field entered in a textbox -->
         <label>Zipcode:</label><input type="text" name="zipcode"><br>
+        
+        <!-- Submit button user clicks to add the information from completed form to the database -->
         <button type="submit" name="submit">Submit</button>
     </form>
 </body>
