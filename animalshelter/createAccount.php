3e55c74ea0e18bb46f34a867f79060cfd8c677ba
uhhsoup <122552993+uhhsoup@users.noreply.github.com>
2023-04-24 16:33:44 -0400

    Add files via upload

--- animalshelter/createAccount.php
+++ animalshelter/createAccount.php
@@ -1,42 +1,43 @@
-<link rel="stylesheet" type="text/css" href="StyleSheets/createAccount.css">
+<link rel="stylesheet" type="text/css" href="StyleSheets/createAccount.css?v=1">
 <form action='createAccountPost.php' method='post' onsubmit='return chk_frm()' name='createAccount'>
-<body>
-	<div id="center-box">
-		<div id="left-box">
-		  <div id="createAccount-box">
-			<div id="shelter-name">Indian Lake Animal Shelter</div>
-			 <input type="text" id="username" name="username" placeholder="Username">
-			 <input type="password" id="password" name="password" placeholder="Password">
-			 <label> Account Type: </label>
-		     <select name="accountType" id="accountType" name="accountType" placeholder="AccountType">
-				<option value=""></option>
-				<option value="Shelter Director">Shelter Director</option>
-				<option value="ACO Officer">ACO Officer</option>
-				<option value="Shelter Personnel">Shelter Personnel</option>
-		     </select><br><br>
-				<input type=submit value="Create Account">
+
+	<body>
+		<div id="center-box">
+			<div id="left-box">
+				<div id="createAccount-box">
+					<div id="shelter-name">Indian Lake Animal Shelter</div>
+					<input type="text" id="username" name="username" placeholder="Username">
+					<input type="password" id="password" name="password" placeholder="Password">
+					<select name="accountType" id="accountType">
+						<option value="" disabled selected>Account Type</option>
+						<option value="Shelter Director">Shelter Director</option>
+						<option value="ACO Officer">ACO Officer</option>
+						<option value="Shelter Personnel">Shelter Personnel</option>
+					</select>
+					</select><br><br>
+					<input type=submit value="Create Account">
+				</div>
+			</div>
+			<div id="right-box">
+				<img src="Images\lizard.jpg" alt="image">
 			</div>
 		</div>
-		<div id="right-box">
-			<img src="Images\lizard.jpg" alt="image">
-		</div>
-	</div>
-</body>
+	</body>
 </form>
 <script>
 	function chk_frm() {
-		if(document.join.user_id.value){
+		if (document.join.user_id.value) {
 			window.alert("ENTER THE USER_ID.");
 			document.join.user_id.focus();
 			return false;
 		}
-		
-		if(document.join.password.value) {
+
+		if (document.join.password.value) {
 			window.alert("ENTER THE PASSWORD.");
 			document.join.password.focus();
 			return false;
 		}
 	}
 	return true;
-	
+
 </script>
--- animalshelter/index.php
+++ animalshelter/index.php
@@ -1,8 +1,10 @@
 <html>
+
 <head>
   <title>Indian Lake Animal Shelter</title>
-  <link rel="stylesheet" type="text/css" href="StyleSheets/login.css">
+  <link rel="stylesheet" type="text/css" href="StyleSheets/login.css?v=1">
 </head>
+
 <body>
   <div id="center-box">
     <div id="left-box">
@@ -11,16 +13,17 @@
         <form action='loginpost.php' method='POST'>
           <input type="text" id="username" name="username" placeholder="Username">
           <input type="password" id="password" name="password" placeholder="Password">
-		  <label> Account Type: </label>
-		  <select name="accountType" id="accountType" name="accountType" placeholder="AccountType">
-            <option value=""></option>
+          <select name="accountType" id="accountType">
+            <option value="" disabled selected>Account Type</option>
             <option value="Shelter Director">Shelter Director</option>
             <option value="ACO Officer">ACO Officer</option>
-			<option value="Shelter Personnel">Shelter Personnel</option>
-        </select><br>
+            <option value="Shelter Personnel">Shelter Personnel</option>
+          </select>
+
+          </select><br>
           <input type="submit" value="Login">
         </form>
-		<br>
+        
         <button onclick="location.href='createAccount.php'">Create Account</button>
       </div>
     </div>
@@ -29,4 +32,5 @@
     </div>
   </div>
 </body>
+
 </html>
