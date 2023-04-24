06a256a91f51f8adb117806ce8d2d89ad276b86e
uhhsoup <122552993+uhhsoup@users.noreply.github.com>
2023-04-24 12:01:33 -0400

    Add files via upload

--- animalshelter/footer.php
+++ animalshelter/footer.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="Footer/footer.css?v=3">
+<link rel="stylesheet" type="text/css" href="StyleSheets/footer.css?v=4">
 
 <footer>
   <p>&copy; 2023 Indian Animal Lake Shelter &bull; thelakeshelter@gmail.com &bull; 905-555-5555 &bull; Copyright @ ScrumMasters</p>
